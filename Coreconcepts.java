872022d8da3dc5f9c46ffffd8eb3422189bd0dbe
Shivaleela34 <153263232+Shivaleela34@users.noreply.github.com>
2025-05-10 11:22:09 +0530

    Add files via upload

---
+++ Coreconcepts.java
@@ -0,0 +1,16 @@
+package techmaster;
+
+public class Coreconcepts {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hi shivaleela"); //display on screen what i wrote in double quotes as it is
+		System.out.printf("hello"); // even by using f also we can display 
+		System.out.print("Arreyyyyyyyyyyyyyyy,,, Helloooo SHIVA....."); // here ln is not used so what we written in double quotes that display continue 
+		System.out.println("Hiiiiiiii");
+		
+
+	}
+
+}
