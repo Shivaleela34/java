e6b443798be749259e53ca580f92e7d3cb7ff177
Shivaleela34 <153263232+Shivaleela34@users.noreply.github.com>
2025-05-10 11:23:02 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,18 @@
+package techmaster;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a=15; //byte data type can take only 2 digits
+		int b=9999; //integer can take more than 4 digits and double also
+		short c=9998; //short can take 4 digits 
+		
+		System.out.println(a); // it will display a value 15,, which is assigned by variable a
+		System.out.println("a"); // it will display the a variable because we written in double quotes
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
